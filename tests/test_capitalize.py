15d81a0c468414269841be49e4615ac48e200023
Nikita Gorynin <nikita12341@mail.ru>
2024-07-26 12:36:14 +0300

    upd test to ASSERT

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,8 @@
-from capitalize import capitalize
+from hexlet_test.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
+# assert capitalize('hello') == 'hello'
 
 print('Все тесты пройдены!')
